e444e5e6a5c30b20ca2fa9cc202e1c49c5e514e6
Christopher Davis <chrisd@gravitybox.com>
2017-12-13 11:52:08 -0500

    build scripts

--- Build/Build.bat
+++ Build/Build.bat
@@ -1,14 +1,14 @@
 RD /S /Q "C:\publish\Datastore"
 mkdir "C:\publish\Datastore"
 
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild.exe" "..\Gravitybox.Datastore.sln" /p:Configuration=Release
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild.exe" "..\Gravitybox.Datastore.sln" /p:Configuration=Release /t:Rebuild
 
 rem COPY SERVICE
 copy "C:\Projects\Gravitybox.Datastore\Gravitybox.Datastore.WinService\bin\Release\*.exe" "C:\publish\Datastore\"
 copy "C:\Projects\Gravitybox.Datastore\Gravitybox.Datastore.WinService\bin\Release\*.dll" "C:\publish\Datastore\"
 copy "C:\Projects\Gravitybox.Datastore\Gravitybox.Datastore.WinService\bin\Release\*.pdb" "C:\publish\Datastore\"
 copy "C:\Projects\Gravitybox.Datastore\Gravitybox.Datastore.WinService\bin\Release\NLog.config" "C:\publish\Datastore\"
-copy "C:\Projects\Gravitybox.Datastore\Gravitybox.Datastore.WinService\bin\Release\InsightGlobal.JobDomain.TaskService.exe.config.orig" "C:\publish\Datastore\"
+copy "C:\Projects\Gravitybox.Datastore\Gravitybox.Datastore.WinService\bin\Release\Gravitybox.Datastore.WinService.exe.config" "C:\publish\Datastore\Gravitybox.Datastore.WinService.exe.config.source"
 
 del /q "C:\publish\Datastore\TaskService\*.vshost.*"
 
